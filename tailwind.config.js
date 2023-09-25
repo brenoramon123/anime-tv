96fd9f2402a5aace8566cd3ab94fcc8c5db1333a
Breno Ramon <108709080+brenoramon123@users.noreply.github.com>
2023-09-24 22:43:17 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        c_dark_1: "#020202",
+        c_gray_1: "#2F2F2F",
+        c_white_1: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
